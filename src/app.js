695b3427a3f6d5034d54b45f46af568e0e114c1e
Aiko <46852063+ItsJustAiko@users.noreply.github.com>
2023-01-10 22:56:58 +0100

    Cleanup code and make better API infrastructure

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
-const express = require('express');
 const app = express();
-const Canvas = require('canvas');
+const bodyParser = require('body-parser');
+const canvas = require('canvas');
+const cors = require('cors');
+const express = require('express');
+const helmet = require('helmet');
 const StackBlur = require('stackblur-canvas');
 
-app.listen(1111, () => {
-    console.log('API listening on port 1111');
+const rateLimiter = rateLimit({
+    windowMs: 60 * 60 * 1000,
+    max: 10000,
+    message: 'Too many requests, try it later',
 });
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+app.use(rateLimiter);
 
 app.get('/api/blur', (req, res) => {
     const url = req.query.url;
@@ -14,9 +25,10 @@ app.get('/api/blur', (req, res) => {
         return res.status(400).json({ error: 'Image url is required' });
     }
 
-    Canvas.loadImage(url)
+    canvas
+        .loadImage(url)
         .then((img) => {
-            const canvas = Canvas.createCanvas(img.width, img.height);
+            const canvas = canvas.createCanvas(img.width, img.height);
             const ctx = canvas.getContext('2d');
             ctx.drawImage(img, 0, 0);
             StackBlur.canvasRGBA(canvas, 0, 0, img.width, img.height, 10);
@@ -28,3 +40,5 @@ app.get('/api/blur', (req, res) => {
             res.status(500).json({ error });
         });
 });
+
+module.exports = app;
